4c1bfffa19c8dc7ac5db277051e52dd26aad2287
Oscar Gariel Naranjo <144272461+onaranjoeng@users.noreply.github.com>
2024-03-25 20:51:45 -0400

    Add files via upload

---
+++ lucas/lucas/lucas.cpp
@@ -0,0 +1,14 @@
+// lucas.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	char s1[20] = "Pikachu";
+	char s2[20] = "Pidgey";
+
+	cout << strcmp(s1, s2);
+}
